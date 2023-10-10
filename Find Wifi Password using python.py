9f077bfdf324fd542b43cbc6d5331cb2fc15724a
Archit Sehgal <68142222+archit-sehgal@users.noreply.github.com>
2023-10-10 19:48:47 +0530

    Update Find Wifi Password using python.py

--- Find Wifi Password using python.py
+++ Find Wifi Password using python.py
@@ -1,78 +1,75 @@
 import subprocess
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 
-# using the check_output() for having the network term retrieval
-devices = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
+def show_loading_screen():
+    loading_window = tk.Toplevel(root)
+    loading_window.title("Loading...")
+    loading_label = tk.Label(loading_window, text="Loading, please wait...")
+    loading_label.pack(padx=20, pady=20)
+    loading_window.update()
+    return loading_window
 
-# decode it to strings
-devices = devices.decode('ascii')
+def hide_loading_screen(loading_window):
+    loading_window.destroy()
 
-s = devices.replace("\r", "")
+def get_network_info():
+    loading_window = show_loading_screen()
+    root.update_idletasks() 
 
-# displaying the information
-nearby = [x[x.find(':') + 1:].replace('\r', '').strip() for x in devices.split('\n') if "SSID" in x]
-
-# getting meta data
-meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
-
-# decoding meta data
-data = meta_data.decode('utf-8', errors="backslashreplace")
-
-# splitting data by line by line
-data = data.split('\n')
-
-# creating a list of profiles
-profiles = []
-
-# traverse the data
-for i in data:
-
-    # find "All User Profile" in each item
-    if "All User Profile" in i:
-        # if found
-        # split the item
-        i = i.split(":")
-
-        # item at index 1 will be the wifi name
-        i = i[1]
-
-        # formatting the name
-        # first and last character is use less
-        i = i[1:-1]
-
-        # appending the wifi name in the list
-        profiles.append(i)
-
-# printing heading
-print("{:<30}| {:<30} | {:<20}".format("Wi-Fi Name", "Password", "Nearby Network"))
-print("-" * 80)
-
-# traversing the profiles
-for i in profiles:
-
-    # try catch block begins
-    # try block
     try:
-        # getting meta data with password using wifi name
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'])
+        devices = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
+        devices = devices.decode('ascii').replace("\r", "")
+        nearby = [x[x.find(':') + 1:].replace('\r', '').strip() for x in devices.split('\n') if "SSID" in x]
 
-        # decoding and splitting data line by line
-        results = results.decode('utf-8', errors="backslashreplace")
-        results = results.split('\n')
+        meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+        data = meta_data.decode('utf-8', errors="backslashreplace")
+        data = data.split('\n')
 
-        # finding password from the result list
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        profiles = []
 
-        # if there is password it will print the pass word
-        try:
-            nearbyFlag = i in nearby
-            print("{:<30}| {:<30} | {:<20}".format(i, results[0], "Y" if nearbyFlag else "N"))
+        for i in data:
+            if "All User Profile" in i:
+                i = i.split(":")[1][1:-1]
+                profiles.append(i)
 
-        # else it will print blank in front of pass word
-        except IndexError:
-            print("{:<30}| {:<}".format(i, ""))
+        result_text.delete(1.0, tk.END) 
+        result_text.insert(tk.END, "{:<30}| {:<30} | {:<20}\n".format("Wi-Fi Name", "Password", "Nearby Network"))
+        result_text.insert(tk.END, "-" * 80 + "\n")
+
+        for i in profiles:
+            try:
+                results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'])
+                results = results.decode('utf-8', errors="backslashreplace").split('\n')
+
+                password = ""
+                for b in results:
+                    if "Key Content" in b:
+                        password = b.split(":")[1].strip()
+                        break
+
+                nearbyFlag = i in nearby
+                result_text.insert(tk.END, "{:<30}| {:<30} | {:<20}\n".format(i, password, "Y" if nearbyFlag else "N"))
+
+            except subprocess.CalledProcessError:
+                result_text.insert(tk.END, "{:<30}| {:<}\n".format(i, ""))
+
+    except Exception as e:
+        result_text.insert(tk.END, "Error: {}\n".format(e))
+    
+    hide_loading_screen(loading_window)
 
 
+root = tk.Tk()
+root.title("Wi-Fi Network Info")
 
-    # called when this process get failed
-    except subprocess.CalledProcessError:
-        print("Encoding Error Occured")
+
+result_text = tk.Text(root, wrap=tk.WORD, width=80, height=20)
+result_text.pack(padx=10, pady=10)
+
+
+get_info_button = ttk.Button(root, text="Get Network Info", command=get_network_info)
+get_info_button.pack()
+
+root.mainloop()
